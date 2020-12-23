e5fbd96cbb550417da7f782be7d0b8f266b31edb
Rodrigo Yael Morales Vázquez <Yava1938@outlook.com>
2020-12-23 07:08:38 -0600

    base de altas rentas diseñado

---
+++ VentaCarros/RentaCarros/Agencia/Rentas/AltaRentas.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AltaRentas.aspx.cs" Inherits="Agencia.Rentas.AltaRentas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Alta de rentas</h2>
+    <hr />
+
+    <div class="container">
+        <div class="row">
+
+            <div class="row form-group">
+            <label for="<%=ddlVehiculo.ClientID %>">Vehiculo:</label>
+            <asp:DropDownList ID="ddlVehiculo" runat="server" CssClass="form-control" style="width:25%">
+                <asp:ListItem Value="0" Text="Selecciona Vehiculo"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ID="rfvddlVehiculo" ValidationGroup="Guardar" runat="server" CssClass="text-danger" ControlToValidate="ddlVehiculo" InitialValue="0" ErrorMessage="Selecciona el vehiculo"></asp:RequiredFieldValidator>
+            </div>
+
+            <div class="row form-group">
+            <label for="<%=ddlCliente.ClientID %>">Cliente:</label>
+            <asp:DropDownList ID="ddlCliente" runat="server" CssClass="form-control" style="width:25%">
+                <asp:ListItem Value="0" Text="Selecciona Cliente"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ID="rfvddlCliente" ValidationGroup="Guardar" runat="server" CssClass="text-danger" ControlToValidate="ddlCliente" InitialValue="0" ErrorMessage="Selecciona el vehiculo"></asp:RequiredFieldValidator>
+            </div>
+
+            <div class="row form-group">
+                <label for="<%=txtNombreCarro.ClientID %>">Duración en días</label>
+                <asp:TextBox ID="txtNombreCarro" runat="server" CssClass="form-control" placeholder="10"></asp:TextBox>
+                <div style="position:absolute;top:0;left:0;">
+                    <asp:RequiredFieldValidator ID="rfvTxtNombreCarro" ValidationGroup="Guardar" 
+                        runat="server" CssClass="text-danger" ControlToValidate="txtNombreCarro" 
+                        ErrorMessage="Nombre del vehiculo requerido"></asp:RequiredFieldValidator>
+                </div>
+            </div>
+
+            <div class="row form-group" style="width:25%">
+            <label for="<%=FechaRenta.ClientID %>">Fecha de Renta:</label>
+            <input id="FechaRenta" runat="server" type="date" class="form-control" />
+                <div style="position: absolute; top: 0; left: 0">
+                <asp:RequiredFieldValidator ID="rfvFechaRenta" ValidationGroup="Guardar" 
+                    runat="server" CssClass="text-danger" ControlToValidate="FechaRenta" 
+                    ErrorMessage="Fecha de renta requerida"></asp:RequiredFieldValidator>
+            </div>
+        </div>
+
+        <div class="row form-group">
+            <asp:Button ID="btnGuardar" ValidationGroup="Guardar" runat="server" Text="Guardar" CssClass="btn btn-success" OnClick="btnGuardar_Click"/>
+        </div>
+        </div>
+
+    </div>
+    <script>
+        $(document).ready(function () {
+            $.datetimepicker.setLocale('es');
+            $("#<%=FechaRenta.ClientID%>").datetimepicker({
+                format: 'm/d/Y H:i',
+                minDate: '0'
+            });
+        });
+    </script>
+</asp:Content>
---
+++ VentaCarros/RentaCarros/Agencia/Rentas/AltaRentas.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Agencia.Rentas {
+    
+    
+    public partial class AltaRentas {
+        
+        /// <summary>
+        /// Control ddlVehiculo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlVehiculo;
+        
+        /// <summary>
+        /// Control rfvddlVehiculo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvddlVehiculo;
+        
+        /// <summary>
+        /// Control ddlCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCliente;
+        
+        /// <summary>
+        /// Control rfvddlCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvddlCliente;
+        
+        /// <summary>
+        /// Control txtNombreCarro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreCarro;
+        
+        /// <summary>
+        /// Control rfvTxtNombreCarro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvTxtNombreCarro;
+        
+        /// <summary>
+        /// Control FechaRenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl FechaRenta;
+        
+        /// <summary>
+        /// Control rfvFechaRenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFechaRenta;
+        
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
